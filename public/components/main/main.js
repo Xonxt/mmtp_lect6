983e141af2f8d5c5cbf65933581632e17865c25a
Xonxt <kov.nikit@gmail.com>
2015-10-30 00:07:46 +0200

    last minute changes

--- public/components/main/main.js
+++ public/components/main/main.js
@@ -4,39 +4,38 @@ var mainModule = angular.module('app.main', ['ngFileUpload']);
 
 mainModule.controller('MainController', ['Upload', '$timeout', MainController]);
 
-function MainController(Upload, $timeout) {	
-	
-	var main = this;
-	
-	main.fileSelected = function(files) {
-		if (files && files.length) {
-			main.file = files[0];
-		} 
-	}
-	
-	main.uploadFiles = function(file) {
-		
-		main.f = file;
-		//main.errFile = errFiles && errFiles[0];
-		
-		if (file) {
-			file.upload = Upload.upload({
-					url: '/upload',
-					data: {file: file}
-			});
-			
-			file.upload.then(function (response) {
-				$timeout(function () {
-						file.result = response.data;
-				});
-			}, function (response) {
-				if (response.status > 0)
-						main.errorMsg = response.status + ': ' + response.data;
-			}, function (evt) {
-				file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-			});
-		}
-		
-	}
-	
+function MainController(Upload, $timeout) {
+
+  var main = this;
+
+  main.fileSelected = function (files) {
+    if (files && files.length) {
+      main.file = files[0];
+    }
+  }
+
+  main.uploadFiles = function (file) {
+
+    main.f = file;
+
+    if (file) {
+      file.upload = Upload.upload({
+        url: '/upload',
+        data: { file: file }
+      });
+
+      file.upload.then(function (response) {
+        $timeout(function () {
+          file.result = response.data;
+        });
+      }, function (response) {
+        if (response.status > 0)
+          main.errorMsg = response.status + ': ' + response.data;
+      }, function (evt) {
+        file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+      });
+    }
+
+  }
+
 }
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -2,33 +2,30 @@ var multer = require('multer');
 var path = require('path');
 
 module.exports = function (app) {	
-	
-	var storage = multer.diskStorage({
-		destination: function (req, file, cb) {
-			cb(null, './public/uploads/');
-		},
-		filename: function (req, file, cb) {			
-			cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-		}
-	})
-	
-	var upload = multer({
-		storage : storage,
-		limits: {fileSize: 500000000, files:1},
-	});
-	
-	var mware = upload.single('file');
-	
-	app.post('/upload', function(req, res) {
-		
-		mware(req, res, function(err) {
-			
-			if(err) {				
-				console.log(err) 
-				console.log(err.stack)				
-			}						
-		});
-		
-	});
-	
+  
+  var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/uploads/');
+    },
+    filename: function (req, file, cb) {			
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  })
+  
+  var upload = multer({
+    storage : storage,
+    limits: {fileSize: 500000000, files:1},
+  });
+  
+  var mware = upload.single('file');
+  
+  app.post('/upload', function(req, res) {
+  
+    mware(req, res, function(err) {    
+      if(err) {				
+        console.log(err) 
+        console.log(err.stack)				
+      }						
+    });  
+  });	
 }
